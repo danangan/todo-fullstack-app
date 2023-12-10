9c35c17f89fe34af12cee98101d37107c2cd399f
Danang Nur <danang.arbansa@gmail.com>
2023-12-10 18:27:27 +1100

    Update model

--- backend/pkg/db/models/todo.go
+++ backend/pkg/db/models/todo.go
@@ -4,10 +4,10 @@ import "github.com/google/uuid"
 
 type Todo struct {
 	BaseModel
-	UserId      uuid.UUID
-	User        User
-	Title       string `gorm:"notNull" validate:"required,alpha,min=3"`
-	Description string `gorm:"notNull" validate:"required,alpha,min=3"`
-	DueDate     string `gorm:"notNull;unique" validate:"required,email"`
-	Password    string `gorm:"notNull" validate:"required,min=6"`
+	UserID      uuid.UUID `gorm:"not null"`
+	User        User      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Title       string    `gorm:"notNull" validate:"required,alpha,min=3"`
+	Description string    `gorm:"notNull" validate:"required,alpha,min=3"`
+	DueDate     string    `gorm:"notNull;unique" validate:"required,email"`
+	Password    string    `gorm:"notNull" validate:"required,min=6"`
 }
